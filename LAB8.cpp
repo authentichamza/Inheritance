4f455166b3f7629ab4deaa99d612f11a87443176
hamza4465989 <43203240+hamza4465989@users.noreply.github.com>
2018-11-28 20:22:09 +0500

    completed

--- LAB8.cpp
+++ LAB8.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include <math.h>
 #include <vector>
 using namespace std;
 class TwoD // base class
@@ -17,8 +18,14 @@ void setY(double NewY){y = NewY;}
 double getX() const {return x;}
 double getY() const {return y;}
 double getDistance(const TwoD& point)const{
-  return sqrt((x*x)+(y*y));
-}
+      double X, Y;
+
+      X = point.getX() - x;
+      Y = point.getY() - y;
+      int a = sqrt((X* X)+(Y*Y));
+      return a;
+
+  }
 };
 class ThreeD : public TwoD
 {
@@ -28,12 +35,25 @@ public:
 ThreeD(double i, double j, double k):TwoD(i,j),z(k){}
 void setZ(double NewZ){z = NewZ;}
 double getZ() const {return z;}
+double getDistance(const ThreeD& point)const;
 };
+double ThreeD::getDistance(const ThreeD& point) const
+{
+    double X,Y,Z;
+    X= point.getX() - x;
+    Y= point.getY() - y;
+    Z=point.getZ() - z;
+    double distance = sqrt((X * X) + (Y * Y) + (Z * Z));
+    return distance;
+}
+
 int main(){
-  //ThreeD d(1,2,3);
+  ThreeD d3(1,2,3);
+  ThreeD d4(4,5,6);
   TwoD d1(1,2);
-  TwoD d2(2,6);
-  cout<<d1.getDistance();
+  TwoD d2(2,4);
+  cout<<d3.getDistance(d4)<<endl;
+  cout <<d4.getDistance(d3)<< endl;
 
 return 0;
 }
